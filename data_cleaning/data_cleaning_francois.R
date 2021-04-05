b3776e899821972cd4d174db69c4ae80db5b404e
François <francois.delavy@gmail.com>
2021-04-05 08:52:16 -0400

    cleaned all question except which-dev-community_33 that require more work

--- data_cleaning/data_cleaning_francois.R
+++ data_cleaning/data_cleaning_francois.R
@@ -43,22 +43,37 @@ cleaning_data <- cleaning_data %>%
     company_onboarding_process_26 = fct_relevel(company_onboarding_process_26, "Yes", "No"), # convert to factor
     improve_onboarding_27 = improve_onboarding_27, # manual work will be needed once we decide if using this or not
     monthly_salary_28 = fct_relevel(monthly_salary_28, 
-                                   "Less than GHS 1,500", 
-                                   "GHS 1,500 - GHS 2,000",
-                                   "GHS 2,000 - GHS 2,500",
-                                   "GHS 2,500 - GHS 3,000",
-                                   "GHS 3,000 - GHS 3,500",
-                                   "GHS 3500 - GHS 4,000",
-                                   "GHS 4,000 - GHS 5,000",
-                                   "GHS 5,000 - GHS 6,000",
-                                   "GHS 6,000 - GHS 8,000",
-                                   "GHS 8,000 - GHS 10,000",
-                                   "GHS 10,000 - GHS 15,000",
-                                   "GHS 15,000 - GHS 20,000",
-                                   "GHS 20,000 - GHS 25,000",
-                                   "Greater than GHS 25,000"
+                                    "Less than GHS 1,500", 
+                                    "GHS 1,500 - GHS 2,000",
+                                    "GHS 2,000 - GHS 2,500",
+                                    "GHS 2,500 - GHS 3,000",
+                                    "GHS 3,000 - GHS 3,500",
+                                    "GHS 3500 - GHS 4,000",
+                                    "GHS 4,000 - GHS 5,000",
+                                    "GHS 5,000 - GHS 6,000",
+                                    "GHS 6,000 - GHS 8,000",
+                                    "GHS 8,000 - GHS 10,000",
+                                    "GHS 10,000 - GHS 15,000",
+                                    "GHS 15,000 - GHS 20,000",
+                                    "GHS 20,000 - GHS 25,000",
+                                    "Greater than GHS 25,000"
     ), # convert to factor and order levels
-    monthly_salary_28 = fct_recode(monthly_salary_28, "GHS 3,500 - GHS 4,000" = "GHS 3500 - GHS 4,000") # correct issue in the levels of the factor
+    monthly_salary_28 = fct_recode(monthly_salary_28, "GHS 3,500 - GHS 4,000" = "GHS 3500 - GHS 4,000"), # correct issue in the levels of the factor
+    job_status_29 = job_status_29, # clean and no NAs
+    drivers_for_new_jobs_30 = drivers_for_new_jobs_30, # We will have to split and count when plotting.
+    how_learn_about_company_31 = how_learn_about_company_31, # We will have to split and count when plotting.
+    dev_community_member_32 = fct_relevel(dev_community_member_32, "Yes", "No"), # convert to factor
+    
+    # a lot to clean on Q 33 if we want to be able to group and count them:
+    which_dev_community_33 = str_replace(which_dev_community_33, "\r\n", ";"),
+    which_dev_community_33 = str_replace(which_dev_community_33, " ,", ";"),
+    which_dev_community_33 = str_replace(which_dev_community_33, ", ", ";"),
+    which_dev_community_33 = str_replace(which_dev_community_33, ",", ";"),
+    
+    which_dev_community_33 = case_when(
+      which_dev_community_33 == "N/A" ~ NA_character_, # else, throw an error. all the values need to be of the same type: char (and not logical)
+      TRUE ~ which_dev_community_33
+    )
     
   ) 
 
